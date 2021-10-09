8a5693a811a2a9df033466eea80cf7b1f36a633e
Hedgy134117 <hedgy134117@gmail.com>
2021-10-08 22:47:24 -0400

    add simple crud app

---
+++ FastAPI Simple SQL App/sql_app/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# ./sql_app.db -> file name
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+# "check_same_thread" -> sqlite requirement
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
